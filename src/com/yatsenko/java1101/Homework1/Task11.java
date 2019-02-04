c7773cd459e93ac6564fa65eac7f9a94f399807b
alonayatsenko <yatsenko.alona@gmail.com>
2019-02-04 22:28:03 +0200

    task11

--- src/com/yatsenko/java1101/Homework1/Task11.java
+++ src/com/yatsenko/java1101/Homework1/Task11.java
@@ -1,4 +1,18 @@
 package com.yatsenko.java1101.Homework1;
 
+import java.util.Scanner;
+
 public class Task11 {
+    public static void main (String [] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        tablUmn(scan.nextInt());
+    }
+    public static void tablUmn(int a) {
+        for (int b=1; b<=10; b++){
+            int result = a*b;
+            System.out.println(a+"*"+b+"=" + result);
+        }
+
+    }
 }
