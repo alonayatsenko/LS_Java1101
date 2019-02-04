b47a55fd9efc2ed6405c4ae6366c18f3992bb5eb
alonayatsenko <yatsenko.alona@gmail.com>
2019-02-04 23:28:44 +0200

    task13

--- src/com/yatsenko/java1101/Homework1/Task13.java
+++ src/com/yatsenko/java1101/Homework1/Task13.java
@@ -1,5 +1,19 @@
 package com.yatsenko.java1101.Homework1;
 
-public class Task13 {
+import java.util.Scanner;
 
-}
+public class Task13 {
+    public static void main (String[]args){
+        System.out.print("Введите номер дня: ");
+        int days = new Scanner(System.in).nextInt();
+
+        convert(days);
+    }
+
+        static void convert (int days){
+        if (days<=0) throw new IllegalArgumentException();
+            System.out.println("В дне прошло:"+days*24+ " часов или"+ days*1440 + " минут или"+ days*86400 + " секунд");
+        }
+    }
+
+
