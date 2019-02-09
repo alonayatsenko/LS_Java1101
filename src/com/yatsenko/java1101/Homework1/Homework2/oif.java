be758ad5aae9e01e571c2eb569c94d15d62abdca
alonayatsenko <yatsenko.alona@gmail.com>
2019-02-09 19:57:05 +0200

    fio4

---
+++ src/com/yatsenko/java1101/Homework1/Homework2/oif.java
@@ -0,0 +1,12 @@
+package com.yatsenko.java1101.Homework1.Homework2;
+
+public class oif {
+    public static void main (String [] args){
+        String fio = "Yatsenko Alona Eduardovna";
+        char [] array = fio.toCharArray();
+        for (int i = array.length -1; i>=0; i--){
+            System.out.print(array[i]);
+        }
+
+    }
+}
