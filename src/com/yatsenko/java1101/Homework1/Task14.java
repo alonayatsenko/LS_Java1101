4ef239ea7287cf46c432aded5529442827d0fc86
alonayatsenko <yatsenko.alona@gmail.com>
2019-02-09 18:00:36 +0200

    task14

---
+++ src/com/yatsenko/java1101/Homework1/Task14.java
@@ -0,0 +1,9 @@
+package com.yatsenko.java1101.Homework1;
+
+public class Task14 {
+    public static void main (String [] args){
+        double grivna = 135;
+        double kurs = 27.4;
+        System.out.println(grivna/kurs + "долларов");
+    }
+}
